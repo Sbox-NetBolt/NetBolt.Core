3d8636827a4a042d1fe0cc205b014741d28ef02b
Peter <petergorman11@gmail.com>
2022-10-12 09:50:31 -0400

    Add verbose logging methods

--- NetBolt.Server/Utility/Log.cs
+++ NetBolt.Server/Utility/Log.cs
@@ -15,7 +15,7 @@ public static class Log
 	/// </summary>
 	private static Logger _logger = null!;
 #endif
-	
+
 	/// <summary>
 	/// Initializes the server-side Serilog logger.
 	/// </summary>
@@ -199,6 +199,107 @@ public static class Log
 		_logger.Fatal( exception, "A fatal exception occurred during runtime" );
 		if ( throwException )
 			throw exception;
+#endif
+	}
+
+	/// <summary>
+	/// Logs a verbose message.
+	/// </summary>
+	/// <param name="message">The message to log.</param>
+	public static void Verbose( string message )
+	{
+#if !NOLOGS
+		_logger.Verbose( "{A}", message );
+#endif
+	}
+
+	/// <summary>
+	/// Logs a verbose message.
+	/// </summary>
+	/// <param name="messageTemplate">The message template.</param>
+	/// <param name="obj">The first object to input to the template.</param>
+	/// <typeparam name="T">The type of <see ref="obj"/>.</typeparam>
+	[MessageTemplateFormatMethod( "messageTemplate" )]
+	public static void Verbose<T>( string messageTemplate, T obj )
+	{
+#if !NOLOGS
+		_logger.Information( messageTemplate, obj );
+#endif
+	}
+
+	/// <summary>
+	/// Logs a verbose message.
+	/// </summary>
+	/// <param name="obj">The first object to log.</param>
+	/// <typeparam name="T">The type of <see ref="obj"/>.</typeparam>
+	public static void Verbose<T>( T obj )
+	{
+#if !NOLOGS
+		_logger.Information( "{A}", obj );
+#endif
+	}
+
+	/// <summary>
+	/// Logs a verbose message.
+	/// </summary>
+	/// <param name="messageTemplate">The message template.</param>
+	/// <param name="obj0">The first object to input to the template.</param>
+	/// <param name="obj1">The second object to input to the template.</param>
+	/// <typeparam name="T1">The type of <see ref="obj0"/>.</typeparam>
+	/// <typeparam name="T2">The type of <see ref="obj1"/>.</typeparam>
+	[MessageTemplateFormatMethod( "messageTemplate" )]
+	public static void Verbose<T1, T2>( string messageTemplate, T1 obj0, T2 obj1 )
+	{
+#if !NOLOGS
+		_logger.Information( messageTemplate, obj0, obj1 );
+#endif
+	}
+
+	/// <summary>
+	/// Logs a verbose message.
+	/// </summary>
+	/// <param name="obj0">The first object to log.</param>
+	/// <param name="obj1">The second object to log.</param>
+	/// <typeparam name="T1">The type of <see ref="obj0"/>.</typeparam>
+	/// <typeparam name="T2">The type of <see ref="obj1"/>.</typeparam>
+	public static void Verbose<T1, T2>( T1 obj0, T2 obj1 )
+	{
+#if !NOLOGS
+		_logger.Information( "{A}\t{B}", obj0, obj1 );
+#endif
+	}
+
+	/// <summary>
+	/// Logs a verbose message.
+	/// </summary>
+	/// <param name="messageTemplate">The message template.</param>
+	/// <param name="obj0">The first object to input to the template.</param>
+	/// <param name="obj1">The second object to input to the template.</param>
+	/// <param name="obj2">The third object to input to the template.</param>
+	/// <typeparam name="T1">The type of <see ref="obj0"/>.</typeparam>
+	/// <typeparam name="T2">The type of <see ref="obj1"/>.</typeparam>
+	/// <typeparam name="T3">The type of <see ref="obj2"/>.</typeparam>
+	[MessageTemplateFormatMethod( "messageTemplate" )]
+	public static void Verbose<T1, T2, T3>( string messageTemplate, T1 obj0, T2 obj1, T3 obj2 )
+	{
+#if !NOLOGS
+		_logger.Information( messageTemplate, obj0, obj1, obj2 );
+#endif
+	}
+
+	/// <summary>
+	/// Logs a verbose message.
+	/// </summary>
+	/// <param name="obj0">The first object to log.</param>
+	/// <param name="obj1">The second object to log.</param>
+	/// <param name="obj2">The third object to log.</param>
+	/// <typeparam name="T1">The type of <see ref="obj0"/>.</typeparam>
+	/// <typeparam name="T2">The type of <see ref="obj1"/>.</typeparam>
+	/// <typeparam name="T3">The type of <see ref="obj2"/>.</typeparam>
+	public static void Verbose<T1, T2, T3>( T1 obj0, T2 obj1, T3 obj2 )
+	{
+#if !NOLOGS
+		_logger.Information( "{A}\t{B}\t{C}", obj0, obj1, obj2 );
 #endif
 	}
 }
